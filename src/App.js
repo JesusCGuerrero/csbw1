cd3751de9229fc02ce919fe2e976ffa5228b6124
Jesus C Guerrero <jesusclementguerrero@gmail.com>
2020-06-25 16:00:13 -0400

    mvp

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import './App.css';
+
+import Header from './components/header';
+import Game from './components/game';
+
+import { CellStampContext } from './context/CellStampContext';
+import { GameColorContext } from './context/GameColorContext';
+import { GameRunningContext } from './context/GameRunningContext';
+import { GameSpeedContext } from './context/GameSpeedContext';
+import { GameStateContext } from './context/GameStateContext';
+
+function App() {
+
+  const start = {
+    'gridSize': 25,
+    'cellSize': 20,
+    'cellLife': new Array(25 * 25).fill(false),
+    'generation': 0
+  }
+  const [cellStamp, setCellStamp] = useState(false)
+  const [gameColor, setGameColor] = useState("#00000")
+  const [gameRunning, setGameRunning] = useState(false)
+  const [gameSpeed, setGameSpeed] = useState(1000)
+  const [gameState, setGameState] = useState(start)
+
+
+
+  return (
+    <div className="App">
+      <CellStampContext.Provider value={{cellStamp, setCellStamp}}>
+        <GameColorContext.Provider value={{gameColor, setGameColor}}>
+          <GameRunningContext.Provider value={{gameRunning, setGameRunning}}>
+            <GameSpeedContext.Provider value={{gameSpeed, setGameSpeed}}>
+              <GameStateContext.Provider value={{gameState, setGameState}}>
+                <Header />
+                <Game />
+              </GameStateContext.Provider>
+            </GameSpeedContext.Provider>
+          </GameRunningContext.Provider>
+        </GameColorContext.Provider>
+      </CellStampContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <div>
+            <h1>Conway's Game of Life</h1>
+        </div>
+    )
+}
+
+export default Header;
